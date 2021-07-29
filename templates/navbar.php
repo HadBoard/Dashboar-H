529d5f101c226e2268f34f9d3442ca35b9e7cf97
Hadi <75636139+HadBoard@users.noreply.github.com>
2021-07-29 16:31:10 +0430

    Update navbar.php

--- templates/navbar.php
+++ templates/navbar.php
@@ -1,1 +1,76 @@
-<?php
+<nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+            <a href="/" class="nav-link">خانه</a>
+        </li>
+    </ul>
+
+    <!-- SEARCH FORM -->
+    <form class="form-inline ml-3" action="https://www.google.com/search" method="get">
+        <div class="input-group input-group-sm">
+            <input class="form-control form-control-navbar" type="search" placeholder="جستجو در گوگل" aria-label="Search" name="q">
+            <div class="input-group-append">
+                <button class="btn btn-navbar" type="submit">
+                    <i class="fa fa-search"></i>
+                </button>
+            </div>
+        </div>
+    </form>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav mr-auto">
+        <!-- Messages Dropdown Menu -->
+        <li class="nav-item dropdown">
+            <a class="nav-link" data-toggle="dropdown" href="#">
+                <i class="fa fa-comments-o"></i>
+                <span class="badge badge-danger navbar-badge">*</span>
+            </a>
+            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-left">
+                <a href="#" class="dropdown-item">
+                    <!-- Message Start -->
+                    <div class="media">
+                        <img src="dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 ml-3 img-circle">
+                        <div class="media-body">
+                            <h3 class="dropdown-item-title">
+                                هادبرد
+                                <span class="float-left text-sm text-danger"><i class="fa fa-star"></i></span>
+                            </h3>
+                            <p class="text-sm">https://hadboard.ir</p>
+                            <p class="text-sm text-muted"><i class="fa fa-clock-o mr-1"></i>چندی قبل</p>
+                        </div>
+                    </div>
+                    <!-- Message End -->
+                </a>
+                <div class="dropdown-divider"></div>
+                <a href="#" class="dropdown-item dropdown-footer">مشاهده همه پیام‌ها</a>
+            </div>
+        </li>
+        <!-- Notifications Dropdown Menu -->
+        <li class="nav-item dropdown">
+            <a class="nav-link" data-toggle="dropdown" href="#">
+                <i class="fa fa-bell-o"></i>
+                <span class="badge badge-warning navbar-badge">*</span>
+            </a>
+            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-left">
+                <span class="dropdown-item dropdown-header">? نوتیفیکیشن</span>
+                <div class="dropdown-divider"></div>
+                <a href="#" class="dropdown-item">
+                    <i class="fa fa-file ml-2"></i> ? گزارش جدید
+                    <span class="float-left text-muted text-sm"الان</span>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a href="#" class="dropdown-item dropdown-footer">مشاهده همه نوتیفیکیشن</a>
+            </div>
+        </li>
+<!--         For change dashboard theme (coming soon ...) -->
+<!--        <li class="nav-item">-->
+<!--            <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#"><i-->
+<!--                    class="fa fa-th-large"></i></a>-->
+<!--        </li>-->
+
+    </ul>
+</nav>
