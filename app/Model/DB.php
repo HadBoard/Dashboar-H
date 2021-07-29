68082dee68e9298c44336b21cddacbeee7f00505
Hadi <75636139+HadBoard@users.noreply.github.com>
2021-07-29 22:31:38 +0430

    Update DB.php

--- app/Model/DB.php
+++ app/Model/DB.php
@@ -1,1 +1,27 @@
 <?php
+
+namespace App\Model;
+
+class DB
+{
+
+    protected $pdo;
+
+    public function __construct()
+    {
+
+        $config = require __DIR__."/../config.php";
+        $database = $config['db']['database'];
+        $username = $config['db']['username'];
+        $password = $config['db']['password'];
+        $host = "localhost";
+        $charset = "UTF-8";
+        $dsn = "mysql:host={$host};dbname={$database};charset={$charset}";
+
+        try {
+            $this->pdo = new \PDO($dsn, $username, $password);
+        } catch (\Exception $ex) {
+            die("Error : {$ex->getMessage()} ");
+        }
+    }
+}
