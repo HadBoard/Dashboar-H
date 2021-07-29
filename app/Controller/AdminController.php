c9a48ba9ad37d9621cd56986ff3e4eeff0004228
Hadi <75636139+HadBoard@users.noreply.github.com>
2021-07-29 23:03:24 +0430

    Update AdminController.php

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -3,5 +3,18 @@
 namespace App\Controller;
 
 class AdminController {
-    
+
+    public function login() {
+
+        if (!isset($_POST['login'])) return;
+
+        $rule = [
+            'email' => 'require|email',
+            'password' => 'require'
+        ];
+
+        if (! $this->validation($_POST, $rule)) return;
+
+    }
+
 }
