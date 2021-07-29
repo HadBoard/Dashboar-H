1fc19f27223d5cef3c1067b21a195d40bbae1773
Hadi <75636139+HadBoard@users.noreply.github.com>
2021-07-29 16:09:13 +0430

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -1,1 +1,58 @@
-<?php
+<!-- written by Seyed Hadi Ranjbar ( HadBoard ) -->
+<!-- https://hadboard.ir -->
+<!-- https://github.com/HadBoard/hashboard -->
+
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title> <?= isset($title) ? $title : "پنل مدیریت" ?> </title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../public/plugins/font-awesome/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="../public/css/adminlte.min.css">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="../public/plugins/iCheck/flat/blue.css">
+    <!-- Morris chart -->
+    <link rel="stylesheet" href="../public/plugins/morris/morris.css">
+    <!-- jvectormap -->
+    <link rel="stylesheet" href="../public/plugins/jvectormap/jquery-jvectormap-1.2.2.css">
+    <!-- Date Picker -->
+    <link rel="stylesheet" href="../public/plugins/datepicker/datepicker3.css">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="../public/plugins/daterangepicker/daterangepicker-bs3.css">
+    <!-- bootstrap wysihtml5 - text editor -->
+    <link rel="stylesheet" href="../public/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+    <!-- Google Font: Source Sans Pro -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+    <!-- bootstrap rtl -->
+    <link rel="stylesheet" href="../public/css/bootstrap-rtl.min.css">
+    <!-- template rtl version -->
+    <link rel="stylesheet" href="../public/css/custom-style.css">
+
+</head>
+<!--
+BODY TAG OPTIONS:
+=================
+Apply one or more of the following classes to to the body tag
+to get the desired effect
+|---------------------------------------------------------|
+|LAYOUT OPTIONS | sidebar-collapse                        |
+|               | sidebar-mini                            |
+|---------------------------------------------------------|
+-->
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+
+    <!-- Navbar -->
+    <?php require_once "navbar.php" ?>
+    <!-- /.navbar -->
+
+    <!-- Main Sidebar Container -->
+    <?php require_once "navbar.php" ?>
+    <!-- .main sidebar container -->
