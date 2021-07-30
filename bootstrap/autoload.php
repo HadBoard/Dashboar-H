a0a2aef07d118e30f02be78d4720f772ec58ba78
Hadi <75636139+HadBoard@users.noreply.github.com>
2021-07-30 11:50:31 +0430

    Update autoload.php

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -4,6 +4,8 @@ ini_set('display_errors', 1);
 ini_set('displcourse_get_dateay_startup_errors', 1);
 error_reporting(1);
 
-session_start();
+if (!session_id()) @session_start();
 
 require_once __DIR__."/../vendor/autoload.php";
+
+$flash = new \Plasticbrain\FlashMessages\FlashMessages();
