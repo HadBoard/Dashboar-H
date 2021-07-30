f95b0c77604dfb435fb7933ee8d6afd6ccf40b62
Hadi <75636139+HadBoard@users.noreply.github.com>
2021-07-30 13:40:15 +0430

    Update functions.php

--- app/functions.php
+++ app/functions.php
@@ -1,5 +1,14 @@
 <?php
 
-function old($field){
-    return $_POST[$field] ?? null;
+function old($field)
+{
+    return request($field);
+}
+
+function request($field = null)
+{
+    $request = new \App\Helper\Request();
+    if (is_null($field))
+        return $request;
+    return $request->input($field);
 }
