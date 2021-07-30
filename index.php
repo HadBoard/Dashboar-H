086c58b3107a7d39475e344d3580d214e64f0ddc
Hadi <75636139+HadBoard@users.noreply.github.com>
2021-07-30 20:21:53 +0430

    Update index.php

--- index.php
+++ index.php
@@ -26,6 +26,7 @@ require_once __DIR__."/templates/header.php";
     <section class="content">
         <div class="container-fluid">
             <!-- Small boxes (Stat box) -->
+
             <div class="row">
                 <div class="col-lg-3 col-6">
                     <!-- small box -->
@@ -83,6 +84,138 @@ require_once __DIR__."/templates/header.php";
                     </div>
                 </div>
                 <!-- ./col -->
+            </div>
+            <!-- /.row -->
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 class="card-title">گزارش سیستم</h5>
+
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-widget="collapse">
+                                    <i class="fa fa-minus"></i>
+                                </button>
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-tool dropdown-toggle" data-toggle="dropdown">
+                                        <i class="fa fa-wrench"></i>
+                                    </button>
+                                    <div class="dropdown-menu dropdown-menu-left" role="menu">
+                                        <a href="#" class="dropdown-item">منو اول</a>
+                                        <a href="#" class="dropdown-item">منو دوم</a>
+                                        <a href="#" class="dropdown-item">منو سوم</a>
+                                        <a class="dropdown-divider"></a>
+                                        <a href="#" class="dropdown-item">لینک</a>
+                                    </div>
+                                </div>
+                                <button type="button" class="btn btn-tool" data-widget="remove">
+                                    <i class="fa fa-times"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <p class="text-center">
+                                        <strong>فروش 1400</strong>
+                                    </p>
+
+                                    <div class="chart">
+                                        <!-- Sales Chart Canvas -->
+                                        <canvas id="salesChart" height="180" style="height: 180px;"></canvas>
+                                    </div>
+                                    <!-- /.chart-responsive -->
+                                </div>
+                                <!-- /.col -->
+                                <div class="col-md-4">
+                                    <p class="text-center">
+                                        <strong>میزان پیشرفت اهداف</strong>
+                                    </p>
+
+                                    <div class="progress-group">
+                                        محصولات اضافه شده به سبد خرید
+                                        <span class="float-left"><b>160</b>/200</span>
+                                        <div class="progress progress-sm">
+                                            <div class="progress-bar bg-primary" style="width: 80%"></div>
+                                        </div>
+                                    </div>
+                                    <!-- /.progress-group -->
+
+                                    <div class="progress-group">
+                                        خرید انجام شده
+                                        <span class="float-left"><b>310</b>/400</span>
+                                        <div class="progress progress-sm">
+                                            <div class="progress-bar bg-danger" style="width: 75%"></div>
+                                        </div>
+                                    </div>
+
+                                    <!-- /.progress-group -->
+                                    <div class="progress-group">
+                                        <span class="progress-text">بازدید صفحات ویژه</span>
+                                        <span class="float-left"><b>480</b>/800</span>
+                                        <div class="progress progress-sm">
+                                            <div class="progress-bar bg-success" style="width: 60%"></div>
+                                        </div>
+                                    </div>
+
+                                    <!-- /.progress-group -->
+                                    <div class="progress-group">
+                                        سوالات ارسالی
+                                        <span class="float-left"><b>250</b>/500</span>
+                                        <div class="progress progress-sm">
+                                            <div class="progress-bar bg-warning" style="width: 50%"></div>
+                                        </div>
+                                    </div>
+                                    <!-- /.progress-group -->
+                                </div>
+                                <!-- /.col -->
+                            </div>
+                            <!-- /.row -->
+                        </div>
+                        <!-- ./card-body -->
+                        <div class="card-footer">
+                            <div class="row">
+                                <div class="col-sm-3 col-6">
+                                    <div class="description-block border-right">
+                                        <h5 class="description-header text-success">تومان 35,210.43</h5>
+                                        <span class="description-text">کل گردش حساب</span>
+                                    </div>
+                                    <!-- /.description-block -->
+                                </div>
+                                <!-- /.col -->
+                                <div class="col-sm-3 col-6">
+                                    <div class="description-block border-right">
+                                        <h5 class="description-header text-warning">تومان 10,390.90</h5>
+                                        <span class="description-text">فروش کل</span>
+                                    </div>
+                                    <!-- /.description-block -->
+                                </div>
+                                <!-- /.col -->
+                                <div class="col-sm-3 col-6">
+                                    <div class="description-block border-right">
+                                        <h5 class="description-header text-success">تومان 24,813.53</h5>
+                                        <span class="description-text">سود کل</span>
+                                    </div>
+                                    <!-- /.description-block -->
+                                </div>
+                                <!-- /.col -->
+                                <div class="col-sm-3 col-6">
+                                    <div class="description-block">
+                                        <h5 class="description-header text-danger">1200</h5>
+                                        <span class="description-text">اهداف</span>
+                                    </div>
+                                    <!-- /.description-block -->
+                                </div>
+                            </div>
+                            <!-- /.row -->
+                        </div>
+                        <!-- /.card-footer -->
+                    </div>
+                    <!-- /.card -->
+                </div>
+                <!-- /.col -->
             </div>
             <!-- /.row -->
         </div><!-- /.container-fluid -->
